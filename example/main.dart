5d55cef91721e127be5e6f331c3ba9b196f206c8
Denis Beketsky <tatsu.ukraine@gmail.com>
2020-02-25 23:50:31 +0200

    added example file

---
+++ example/main.dart
@@ -0,0 +1,8 @@
+/// 1. Install your project dependencies
+///
+/// 2. Run `flutter pub run environment_config:generate` with key and values specified
+/// in your YAML config
+///
+/// 3. Import generated file
+
+import 'environment_config.dart';
